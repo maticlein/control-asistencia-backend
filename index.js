11d147ec2c45382a6a2ef752bed1caf20f565c41
Matthias Clein <m.cleinespinoza@gmail.com>
2021-11-24 10:19:29 -0300

    Teachable Machine first test

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
+require('dotenv').config()
 const express = require('express');
+const TeachableMachine = require('@sashido/teachablemachine-node'); 
+
+const model = new TeachableMachine({
+    modelUrl: process.env.TM_MODEL
+});
+
 const app = express();
-const port = 3000;
+app.use(express.json());
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Control de asistencia');
+});
+
+app.get('/image/classify/', async (req, res) => {
+    console.log(req.body.imageUrl);
+    return model.classify({
+        imageUrl: req.body.imageUrl,
+    }).then((predictions) => {
+        console.log(predictions);
+        return res.json(predictions[0]);
+    }).catch((e) => {
+        console.error(e);
+        res.status(500).send("Algo ha salido mal!")
+    })
 });
 
 app.listen(port, () => {
